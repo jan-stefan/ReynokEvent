d97d11f839d7d162cdfdbae4acaf561ceb24cd35
Jan Stefan Gehrmann <gehrmann.janstefan@gmail.com>
2020-02-15 21:04:39 +0100

    Changed structure and added some information in package.json

--- src/main/ReynokEvent.js
+++ src/main/ReynokEvent.js
@@ -118,3 +118,4 @@ const ReynokEvent = {
     }
 };
 
+module.exports = ReynokEvent;
---
+++ src/test/ReynokEvent.js
@@ -0,0 +1,22 @@
+// File for performing tests...
+// todo: make runable as auto testscript
+
+
+//use subscribe call to subscribe to a event.
+// import {ReynokEvent} from "./src/main/ReynokEvent";
+// var ReynokEvent = require("./src/main/ReynokEvent.js");
+
+// ReynokEvent.subscribe('mach', function () {
+//     console.log("luuuui");
+// });
+//
+// // you can cahe the eventId to unsubscribe later.
+// const eventIdFromLui1 = ReynokEvent.subscribe('mach', function () {
+//     console.log("luuuui1");
+// });
+//
+// // call emit to trigger the event and its functionality.
+// ReynokEvent.emit('mach');
+//
+// // unsubscribe using the eventid
+// ReynokEvent.unsubscribe(eventIdFromLui1);
