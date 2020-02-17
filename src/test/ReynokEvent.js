ee73b545dea1ce9195be3fcde2aa51b6e22a83dd
Mirco Tries <mirco.tries@1und1.de>
2020-02-17 14:21:24 +0100

    Added QUnit and some tests, reworked bad code practices

--- src/test/ReynokEvent.js
+++ src/test/ReynokEvent.js
@@ -1,22 +1,45 @@
-// File for performing tests...
-// todo: make runable as auto testscript
+const reynokEvent = require('../main/ReynokEvent');
 
+const TEST_EVENT_NAME = "my-test-event";
 
-//use subscribe call to subscribe to a event.
-// import {ReynokEvent} from "reynok-event";
-// var ReynokEvent = require("reynok-event");
+QUnit.test('test event it not equal for 100 generations', assert => {
+    let ids = [];
 
-// ReynokEvent.subscribe('mach', function () {
-//     console.log("luuuui");
-// });
-//
-// // you can cahe the eventId to unsubscribe later.
-// const eventIdFromLui1 = ReynokEvent.subscribe('mach', function () {
-//     console.log("luuuui1");
-// });
-//
-// // call emit to trigger the event and its functionality.
-// ReynokEvent.emit('mach');
-//
-// // unsubscribe using the eventid
-// ReynokEvent.unsubscribe(eventIdFromLui1);
+    for (let i = 0; i < 100; i++) {
+        let id = reynokEvent.__createEventId();
+
+        if (!ids.includes(id)) {
+            ids.push(id);
+        }
+    }
+
+    assert.equal(ids.length, 100, "100 unique IDS should be found");
+});
+
+QUnit.test('subscribe and emit', assert => {
+    reynokEvent.clear();
+    assert.expect(1);
+
+    let done = assert.async();
+
+    reynokEvent.subscribe(TEST_EVENT_NAME, () => {
+        assert.ok(true, 'Event was called.');
+        done()
+    });
+
+    reynokEvent.emit(TEST_EVENT_NAME);
+});
+
+QUnit.test('subscribe and unsubscribe', assert => {
+    reynokEvent.clear();
+    assert.expect(2);
+
+    let eventId = reynokEvent.subscribe(TEST_EVENT_NAME, () => {
+    });
+
+    reynokEvent.unsubscribe(eventId);
+
+    assert.ok(true, 'Unsubscribe worked.');
+
+    assert.throws(() => reynokEvent.unsubscribe(eventId));
+});
